aece536d9eeeed90a10e015c9c3c8b6f06a8364e
György Kiss <kissgyorgy@me.com>
2024-03-08 00:20:34 +0100

    stories: Refact renderer

--- stories/compone_stories/renderer.py
+++ stories/compone_stories/renderer.py
@@ -1,54 +1,72 @@
 import importlib
 import inspect
 import multiprocessing as mp
-from multiprocessing.connection import Connection
-from pathlib import Path
+from multiprocessing.context import SpawnProcess
 
 from .stories import Story, is_story
 
 
-def init_stories(module_names: list[str]) -> dict[str, Story]:
-    print("Got modules", module_names)
-    importlib.invalidate_caches()
-    module_names = [importlib.import_module(path) for path in module_names]
-    all_stories = {}
-    for mod in module_names:
-        story_objects = inspect.getmembers(mod, predicate=is_story)
-        storymap = {story.get_name(): story for _, story in story_objects}
-        all_stories.update(storymap)
-    print("Imported stories:", all_stories.keys())
-    return all_stories
-
-
-class Command:
+class _Command:
     RENDER = "render"
     STORY_NAMES = "story_names"
     STOP = "stop"
 
 
-def start_process(modules: list[Path], conn: Connection):
-    stories = init_stories(modules)
+class _RenderProcess(SpawnProcess):
+    daemon = True
 
-    while True:
-        msg = conn.recv()
-        print("Got message", msg)
-        cmd, *args = msg
+    def __init__(self, modules, conn):
+        super().__init__()
+        self._stories = []
+        self._module_names = modules
+        self._conn = conn
+        self._running = False
 
-        if cmd == Command.RENDER:
+    def start(self):
+        self._running = True
+        super().start()
+
+    def run(self):
+        self._init_stories()
+
+        while self._running:
+            msg = self._conn.recv()
+            print("Got message", msg)
+
+            cmd, *args = msg
+            if res := self._run_command(cmd, *args):
+                self._conn.send(res)
+
+        print("_RenderProcess stopped.")
+
+    def _init_stories(self) -> dict[str, Story]:
+        print("Got modules", self._module_names)
+        importlib.invalidate_caches()
+        modules = [importlib.import_module(name) for name in self._module_names]
+
+        all_stories = {}
+        for mod in modules:
+            story_objects = inspect.getmembers(mod, predicate=is_story)
+            storymap = {story.get_name(): story for _, story in story_objects}
+            all_stories.update(storymap)
+
+        print("Imported stories:", all_stories.keys())
+        self._stories = all_stories
+
+    def _run_command(self, cmd, *args):
+        if cmd == _Command.RENDER:
             story_name = args[0]
-            comp_obj = stories[story_name].component()
+            comp_obj = self._stories[story_name].component()
             content = str(comp_obj)
-            conn.send(content)
-        elif cmd == Command.STORY_NAMES:
-            story_names = list(stories.keys())
-            conn.send(story_names)
-        elif cmd == Command.STOP:
-            conn.close()
-            break
+            return content
+        elif cmd == _Command.STORY_NAMES:
+            story_names = list(self._stories.keys())
+            return story_names
+        elif cmd == _Command.STOP:
+            self._conn.close()
+            self._running = False
         else:
-            raise ValueError(f"Unknown message: {msg}")
-
-    print("Renderer stopped.")
+            raise ValueError(f"Unknown command: {cmd}")
 
 
 class Renderer:
@@ -63,27 +81,27 @@ class Renderer:
         self._process = None
 
     def start(self):
-        ctx = mp.get_context("spawn")
-        self._parent_conn, self._child_conn = ctx.Pipe()
-        self._process = ctx.Process(
-            target=start_process, args=(self._modules, self._child_conn)
-        )
-        self._process.daemon = True
+        self._parent_conn, self._child_conn = mp.Pipe()
+        self._process = _RenderProcess(self._modules, self._child_conn)
         self._process.start()
 
     def _run_command(self, command, *args):
         self._parent_conn.send([command, *args])
         return self._parent_conn.recv()
 
+    def render_story(self, story_name: str) -> str:
+        return self._run_command(_Command.RENDER, story_name)
+
+    def story_names(self) -> list[str]:
+        return self._run_command(_Command.STORY_NAMES)
+
     def stop(self):
         # Must not recv(), otherwise it will hang
-        self._parent_conn.send([Command.STOP])
+        self._parent_conn.send([_Command.STOP])
         self._parent_conn.close()
+
         self._process.join()
         self._process.close()
 
-    def render_story(self, story_name: str) -> str:
-        return self._run_command(Command.RENDER, story_name)
-
-    def story_names(self) -> list[str]:
-        return self._run_command(Command.STORY_NAMES)
+        self._parent_conn, self._child_conn = None, None
+        self._process = None
