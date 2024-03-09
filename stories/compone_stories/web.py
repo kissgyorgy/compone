a12c081ef6a068ac67467174970501c41b9476c4
György Kiss <kissgyorgy@me.com>
2024-03-09 15:26:13 +0100

    stories: return components from views directly
    Make it possible to return component instances without
    having to convert to str first.

--- stories/compone_stories/web.py
+++ stories/compone_stories/web.py
@@ -1,11 +1,21 @@
-from flask import Flask, redirect, url_for
+from compone.component import _ComponentBase
+from flask import Flask, Response, redirect, url_for
+from flask import typing as ft
 
 from .components import Page
 from .renderer import Renderer
 
 
+class ComponentApp(Flask):
+    def make_response(self, rv: ft.ResponseReturnValue) -> Response:
+        if isinstance(rv, _ComponentBase):
+            return self.response_class(str(rv))
+        else:
+            return super().make_response(rv)
+
+
 def create_app(modules: list[str]):
-    app = Flask("compone_stories")
+    app = ComponentApp("compone_stories")
     renderer = Renderer(modules)
 
     @app.route("/")
@@ -17,12 +27,10 @@ def create_app(modules: list[str]):
     def story(story_name):
         story_names = renderer.story_names()
         story_content = renderer.render_story(story_name)
-        return str(
-            Page(
-                story_names=story_names,
-                story_content=story_content,
-                active_story=story_name,
-            )
+        return Page(
+            story_names=story_names,
+            story_content=story_content,
+            active_story=story_name,
         )
 
     renderer.start()
